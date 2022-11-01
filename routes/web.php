045c5c23ae5f9140137516d106d39322ff83526c
Kolya Mykhalko <mykhalko98@gmail.com>
2022-11-01 16:08:25 +0200

    tmp

--- routes/web.php
+++ routes/web.php
@@ -83,7 +83,7 @@ Route::resource('stock', StockController::class);
 // Route::resource('subscription', SubscriptionController::class);
 Route::resource('setting', SettingController::class);
 Route::get('/setting/vouchers/template', [SettingController::class, 'voucherTemplate']);
-    Route::get('/setting/tickets/template', [SettingController::class, 'ticketTemplate']);
+Route::get('/setting/tickets/template', [SettingController::class, 'ticketTemplate']);
 Route::resource('dashboard', DashboardController::class);
 // Route::resource('reservation', ReservationController::class);
 // Route::resource('scheme', SchemeController::class);

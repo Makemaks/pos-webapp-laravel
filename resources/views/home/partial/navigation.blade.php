3b8b03cffae443fec5351344875d917f85aecad2
Ronald Ugiomoh <ronaldugiomoh@gmail.com>
2023-01-24 16:00:33 +0000

    setting menu level-Builder

--- resources/views/home/partial/navigation.blade.php
+++ resources/views/home/partial/navigation.blade.php
@@ -9,7 +9,7 @@
     <div>
         <div class="uk-search uk-search-default">
             <span uk-search-icon></span>
-            <input class="uk-search-input" type="search" {{-- onclick="showKeypad()"  --}}onchange="searchInput(this)" autocomplete="off">
+            <input class="uk-search-input" type="search" {{-- onclick="showKeypad()"  --}}onkeydown="searchInput(this)" autocomplete="off">
         </div>
     </div>
    

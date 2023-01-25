ddda9acdd34ce4a3b3d68b7c8d65532781550d7c
Ronald Ugiomoh <ronaldugiomoh@gmail.com>
2023-01-25 14:58:56 +0000

    menu-allocation

--- database/migrations/2022_04_07_173903_create_setting_table.php
+++ database/migrations/2022_04_07_173903_create_setting_table.php
@@ -68,7 +68,7 @@ return new class extends Migration
             $table->json('setting_building')->nullable()->comment('address_id::status::capacity::name::description::note[]');
             $table->json('setting_room')->nullable()->comment('setting_building_id::status::capacity::name::description::size[]::section[]::note[]');
             
-            $table->json('setting_menu_allocation')->nullable()->comment('button_id::level_id::record_id::font_id::style_id::text_color, background_color');
+            $table->json('setting_menu_allocation')->nullable()->comment('button_id::level_id::record_id::font::style_id::text_color, background_color');
 
             //$table->json('setting_template')->nullable()->comment('template_id::templatetable_id::templatetable_type::name::company_name'); 
            
